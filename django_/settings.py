a3ad98101b53f9f23db599b0ad5aee082d47d835
João Pedro Vitorino <joaovitorino@unigranrio.br>
2023-05-09 20:47:55 -0300

    Testando online

--- django_/settings.py
+++ django_/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-x13@ulyo7z5#w42t^#g8pnmlc*%n2)z=t&#r%2mem9wa74%3x3
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = ['https://peekky.com', 'https://web-production-870f.up.railway.app','https://www.peekky.com']
 # Application definition
 
 INSTALLED_APPS = [
