cec2603d8d9b253507f920d67c5ed0b176127458
João Pedro Vitorino <joaovitorino@unigranrio.br>
2023-05-09 18:47:15 -0300

    Testando online

--- django_/settings.py
+++ django_/settings.py
@@ -138,7 +138,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = (BASE_DIR / 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
