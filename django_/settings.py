e64bd46f43349d721cd591f7b815cd8bbb475058
João Pedro Vitorino <joaovitorino@unigranrio.br>
2023-05-09 19:12:28 -0300

    Testando online

--- django_/settings.py
+++ django_/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-x13@ulyo7z5#w42t^#g8pnmlc*%n2)z=t&#r%2mem9wa74%3x3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
